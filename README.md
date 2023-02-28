7d80a7843144ab0d80cbd813c112e08e79a429cd
mmsharepoint <markus.moeller.ed@outlook.de>
2023-02-28 20:34:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# tab-sso-graph-file-conversion
+
+## Summary
+This Teams Tab enables a user to upload a supported (depending on the target) file type via drag and drop while the uploaded file will be converted as PDF, HTML, JPG depending on user's selection.
+In Teams context (as configurable Tab) it uses the current Teams' site and the default drive. In case of a personal tab the SharePoint root site's default drive'
+It uses the following capabilities (mostly) on behalf of Microsoft Graph:
+* Use HTML5 drag and drop event handling
+* Writing normal files smaller 4MB
+* Writing files bigger than 4MB with a resumable upload
+* Retrieving files with format=pdf/html/jpg conversion which is currently supportred only with Graph beta endpoint
+
+![File upload and PDF conversion](TabSSOGraphUploadPDF/assets/01UploadProcess.gif)
+
+For further details see the author's [blog post](https://mmsharepoint.wordpress.com/2022/)
+
+## Applies to
+
+This sample was created [using the Teams Toolkit with Visual Studio 2022](https://learn.microsoft.com/en-us/microsoftteams/platform/toolkit/teams-toolkit-fundamentals?pivots=visual-studio&WT.mc_id=M365-MVP-5004617). The sample was also realized with Graph v1.0 enpoint as [PDF only conversion](https://github.com/mmsharepoint/tab-sso-graph-upload-pdf-csharp) and can be found [here](https://github.com/mmsharepoint/tab-sso-graph-upload-as-pdf).
+
+## Version history
+
+Version|Date|Author|Comments
+-------|----|--------|--------
+1.0|Mar 14, 2023|[Markus Moeller](http://www.twitter.com/moeller2_0)|Initial release
+
+## Disclaimer
+
+**THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.**
+
+---
+## Minimal Path to Awesome
+- Clone the repository
+    ```bash
+    git clone https://github.com/mmsharepoint/tab-mgt-person-csharp.git
+- Perform actions in GettingStarted.txt
+- You will need to register an app in Azure AD [Can also be accomplished by Visual Studio](https://learn.microsoft.com/en-us/microsoftteams/platform/toolkit/add-single-sign-on?pivots=visual-studio&WT.mc_id=M365-MVP-5004617#add-sso-to-teams-app-for-visual-studio)
+  - with redirect uri https://localhost/blank-auth-end.html
+  - Make it multi-tenant
+  - with client secret
+  - with **delegated** permissions Files.ReadWrite and Sites.ReadWrite.All
+  - With exposed Api "access_as_user" and App ID Uri api://localhost/<App ID>
+  - With the client IDs for Teams App and Teams Web App 1fec8e78-bce4-4aaf-ab1b-5451cc387264 and 5e3ce6c0-2b1f-4285-8d4b-75ee78787346
+- Add the app registration ClientId, ClientSecret to your appsettings.json (or a appsettings.Development.json)
+- Fill OAuthAuthority with https://login.microsoftonline.com/_YOUR_TENANTID_
+
+## Features
+This is a simple Teams Tab. It uses the Microsoft Graph Toolkit (MGT) for simplifying access to Microsoft 365 resources.
+* SSO access token generation to access Microsoft Graph
+* Call backend controller to execute O-B-O flow and Microsoft Graph requests
+* [Upload large files with Microsoft Graph](https://learn.microsoft.com/en-us/graph/sdks/large-file-upload?tabs=csharp?WT.mc_id=M365-MVP-5004617)
+* [Convert supported files to various target formats with Microsoft Graph](https://learn.microsoft.com/en-us/graph/api/driveitem-get-content-format?view=graph-rest-beta&tabs=http&WT.mc_id=M365-MVP-5004617)
+* [Use Fluent UI Web Components with Blazor](https://learn.microsoft.com/en-us/fluent-ui/web-components/integrations/blazor?WT.mc_id=M365-MVP-5004617)
---
+++ TabSSOGraphUploadPDF/wwwroot/js/TabFile.js
@@ -0,0 +1,147 @@
+﻿(function (TabFile, $, undefined) {
+  ssoToken = "";
+  siteUrl = "";
+  selectedFilyType = "PDF";
+
+  TabFile.getSSOToken = function () {
+    if (microsoftTeams) {
+      microsoftTeams.initialize();
+      microsoftTeams.authentication.getAuthToken({
+        successCallback: (token, event) => {
+          console.log(token);
+          TabFile.ssoToken = token;
+          
+        },
+        failureCallback: (error) => {
+          renderError(error);
+        }
+      });
+    }
+  }
+
+  TabFile.getContext = function () {
+    if (microsoftTeams) {
+      microsoftTeams.app.getContext()
+        .then(context => {
+          if (context.sharePointSite.teamSiteUrl !== "") {
+            TabFile.siteUrl = context.sharePointSite.teamSiteUrl;
+          }
+          else {
+            TabFile.siteUrl = "https://" + context.sharePointSite.teamSiteDomain;
+          }
+        });
+    }   
+  }
+
+  TabFile.executeUpload = function (event) {
+    TabFile.Drag.allowDrop(event);
+    const loaderDIV = document.getElementsByClassName('loader')[0];
+    loaderDIV.style.display = 'flex';
+    const dt = event.dataTransfer;
+    const files = Array.prototype.slice.call(dt.files); // [...dt.files];
+    files.forEach(fileToUpload => {
+      const extensions = fileToUpload.name.split('.');
+      const fileExtension = extensions[extensions.length - 1];
+      TabFile.Drag.disableHighlight(event);
+      if (TabFile.Utilities.isFileTypeSupported(fileExtension, TabFile.selectedFilyType)) {
+        const formData = new FormData();
+        formData.append('file', fileToUpload);
+        formData.append('Name', fileToUpload.name);
+        formData.append('SiteUrl', TabFile.siteUrl);
+        formData.append('TargetType', TabFile.selectedFilyType);
+
+        fetch("/api/Upload", {
+          method: "post",
+          headers: {
+            "Authorization": "Bearer " + TabFile.ssoToken,
+            // "Content-Type": "multipart/form-data; boundary=--WebKitFormBoundaryfgtsKTYLsT7PNUVD"
+          },
+          body: formData
+        })
+        .then((response) => {
+          response.text().then(resp => {
+            console.log(resp);
+            TabFile.addConvertedFile(resp);
+            loaderDIV.style.display = 'none';
+          });
+        });
+      }
+      else {
+        alert('File type not supported!')
+      }
+    });
+  }
+
+  TabFile.addConvertedFile = function (fileUrl) {
+    const parentDIV = document.getElementsByClassName('dropZoneBG');
+    const fileLineDIV = document.createElement('div');
+    fileLineDIV.innerHTML = '<span>File uploaded to target and available <a href=' + fileUrl + '> here.</a ></span > ';
+    parentDIV[0].appendChild(fileLineDIV);
+  }
+
+  TabFile.fileTypeChanged = function (event) {
+    const fileType = event.target.value;
+    TabFile.changeFileType(fileType);
+  }
+
+  TabFile.changeFileType = function (fileType) {
+    TabFile.selectedFilyType = fileType.toUpperCase();
+    const span = document.getElementById('fileTypes');
+    span.innerText = TabFile.Utilities.getSupportedFileTypes(fileType.toUpperCase());
+    const icon = document.getElementById('fileIcon');
+    switch (fileType.toUpperCase()) {
+      case 'PDF':
+        icon.className = 'ms-Icon ms-Icon--PDF pdfLogo';
+        break;
+      case 'HTML':
+        icon.className = 'ms-Icon ms-Icon--FileHTML pdfLogo';
+        break;
+      case 'JPG':
+        icon.className = 'ms-Icon ms-Icon--PictureCenter pdfLogo';
+        break;
+    }
+  }
+  /// Class 'Drag' for TabFile
+  TabFile.Drag = {};
+  {
+    TabFile.Drag.allowDrop = function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      event.dataTransfer.dropEffect = 'copy';
+    }
+
+    TabFile.Drag.enableHighlight = function (event) {
+      TabFile.Drag.allowDrop(event);
+      const bgDIV = document.getElementsByClassName('dropZone');
+      bgDIV[0].classList.add('dropZoneHighlight');
+    }
+
+    TabFile.Drag.disableHighlight = function (event) {
+      TabFile.Drag.allowDrop(event);
+      const bgDIV = document.getElementsByClassName('dropZone');
+      bgDIV[0].classList.remove('dropZoneHighlight');
+    }
+  }
+
+  TabFile.Utilities = {};
+  {
+    const supportedFileTypes = {
+      PDF: ['csv', 'doc', 'docx', 'odp', 'ods', 'odt', 'pot', 'potm', 'potx', 'pps', 'ppsx', 'ppsxm', 'ppt', 'pptm', 'pptx', 'rtf', 'xls', 'xlsx'],
+      HTML: ['eml', 'md', 'msg'],
+      JPG: ['3g2', '3gp', '3gp2', '3gpp', '3mf', 'ai', 'arw', 'asf', 'avi', 'bas', 'bash', 'bat', 'bmp', 'c', 'cbl', 'cmd', 'cool', 'cpp', 'cr2', 'crw', 'cs', 'css', 'csv', 'cur', 'dcm', 'dcm30', 'dic', 'dicm', 'dicom', 'dng', 'doc', 'docx', 'dwg', 'eml', 'epi', 'eps', 'epsf', 'epsi', 'epub', 'erf', 'fbx', 'fppx', 'gif', 'glb', 'h', 'hcp', 'heic', 'heif', 'htm', 'html', 'md', 'mov', 'mp4', 'pdf', 'png', 'ppt', 'pptx', 'rtf', 'tiff', 'txt', 'wmv', 'xml']
+    };
+
+    TabFile.Utilities.getSupportedFileTypes = function (srcType) {
+      return supportedFileTypes[srcType].join(', ');
+    }
+
+    TabFile.Utilities.isFileTypeSupported = function (srcType, trgType) {
+      if (supportedFileTypes[trgType.toUpperCase()].indexOf(srcType.toLowerCase()) > -1) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+  }
+}(window.TabFile = window.TabFile || {}));
